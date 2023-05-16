7d1cadd6372f82dd9a7d39c83e9ae035cd8ed25b
HusarOlha <husarolya@gmail.com>
2023-05-16 13:42:32 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
+import React from 'react';
+
 import { Routes, Route } from 'react-router-dom';
-import MovieDetails from 'pages/MovieDetails';
-import Cast from './Cast/Cast';
+import { lazy } from 'react';
 import Layout from './Layout/Layout';
-import Reviews from './Reviews/Reviews';
-import Movies from 'pages/Movies';
+
 import { Toaster } from 'react-hot-toast';
-import Home from 'pages/Home';
+
+const Home = lazy(() => import('pages/Home'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Movies = lazy(() => import('pages/Movies'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Toaster
         position="top-right"
         toastOptions={{
@@ -24,19 +29,17 @@ export const App = () => {
           },
         }}
       ></Toaster>
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="movies" element={<Movies />} />
-            <Route path="movies/:movieId" element={<MovieDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<div>Not found</div>} />
-        </Routes>
-      </>
-    </div>
+        </Route>
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </>
   );
 };
