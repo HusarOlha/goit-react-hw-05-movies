2db182b8e53c778e6cfce34cd66e3124d0536abc
HusarOlha <husarolya@gmail.com>
2023-05-16 11:36:04 +0300

    add

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -12,7 +12,7 @@ const Cast = () => {
 
   useEffect(() => {
     setLoading(true);
-
+    const abortController = new AbortController();
     const fetchMovieCast = async () => {
       try {
         const response = await GetCast(movieId);
@@ -25,6 +25,9 @@ const Cast = () => {
     };
 
     fetchMovieCast();
+    return () => {
+      abortController.abort();
+    };
   }, [movieId]);
 
   if (loading) {
